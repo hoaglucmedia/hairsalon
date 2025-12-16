b1f49360fc7a419aa442cb2479a66df46272df35
HoangLuc123 <123654261+HoangLuc123@users.noreply.github.com>
2025-12-16 10:32:13 +0700

    Fix database config for local & InfinityFree

--- config.php
+++ config.php
@@ -1,24 +1,32 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-session_start();
-
-
-$servername = "localhost";
-$username_db = "root";
-$password_db = "";
-$dbname = "hairsalon";
+// Nếu đang chạy trên InfinityFree
+if (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'epizy.com') !== false) {
+    // --- INFINITYFREE ---
+    $servername  = "sql305.infinityfree.com";
+    $username_db = "if0_40487423";
+    $password_db = "XLHYE6gs94La";
+    $dbname      = "if0_40487423_shop";
+} else {
+    // --- LOCALHOST (XAMPP) ---
+    $servername  = "localhost";
+    $username_db = "root";
+    $password_db = "";
+    $dbname      = "hairsalon";
+}
 
 try {
-  
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username_db, $password_db);
-    
-    
+    $conn = new PDO(
+        "mysql:host=$servername;dbname=$dbname;charset=utf8",
+        $username_db,
+        $password_db
+    );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    
-} catch(PDOException $e) {
- 
-    die("Kết nối thất bại: " . $e->getMessage());
+} catch (PDOException $e) {
+    die("Lỗi kết nối cơ sở dữ liệu.");
 }
 ?>
